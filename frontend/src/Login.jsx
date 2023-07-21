dd24a946ccb7020a0007021e0cfd1f3702b905fb
mar19a <mariano@bu.edu>
2023-07-20 23:24:03 -0400

    Working on the dashboard

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,14 +1,28 @@
 import React, { useState } from 'react';
 import './style.css'
-
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 function Login() {
     const [values, setValues] = useState({ email: '', password: '' });
-    const [error, setError] = useState(null);
+    const navigate = useNavigate()
+ 
+    const [error, setError] = useState('');
 
     const handleSubmit = (event) => {
         event.preventDefault();
         // add your login logic here
-        console.log(values);
+       
+        axios.post('http://localhost:8081/login', values)
+        .then(res => 
+            {
+                if(res.data.Status == 'Success')
+                {
+                    navigate('/');
+                } else{
+                    setError(res.data.Error);
+                }
+            })
+        .catch(err => console.log(err));
     };
 
     return (
@@ -22,14 +36,15 @@ function Login() {
                     <div className='mb-3'>
                         <label htmlFor="email" className="text-white"><strong>Email</strong></label>
                         <input type="email" placeholder='Enter Email' name='email' 
-                          onChange={e => setValues({...values, email: e.target.value})} className='form-control rounded-0 text-white' autoComplete='off'/>
+    onChange={e => setValues({...values, email: e.target.value})} className='form-control rounded-0 input-text' autoComplete='off'/>
                     </div>
                     <div className='mb-3'>
                         <label htmlFor="password" className="text-white"><strong>Password</strong></label>
                         <input type="password" placeholder='Enter Password' name='password'
-                          onChange={e => setValues({...values, password: e.target.value})} className='form-control rounded-0 text-white' />
+    onChange={e => setValues({...values, password: e.target.value})} className='form-control rounded-0 input-text' />
                     </div>
-                    <button type='submit' className='btn btn-success w-100 rounded-0'> Log in</button>
+                    <button type='submit' className='btn btn-success w-100 rounded-0 mb-2'> Log in</button>
+                   
                     <p className="text-white">You agree to our terms and policies</p>
                 </form>
             </div>
