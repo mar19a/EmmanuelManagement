3505853f462a9f96680432e08ba3ab2595564e7e
mar19a <mariano@bu.edu>
2023-07-24 22:32:47 -0400

    Change element

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
       <Route path = '/employee' element = {<Employee />}></Route>
       <Route path = '/profile' element = {<Profile />}></Route>
       <Route path='/create' element={<AddEmployee />}></Route>
-      <Route path='/employeeEdit:id' element={<AddEmployee />}></Route>
+      <Route path='/employeeEdit/:id' element={<EditEmployee />}></Route>
       </Route>
       <Route path = '/Login' element = {<Login />}></Route>
     </Routes>  
