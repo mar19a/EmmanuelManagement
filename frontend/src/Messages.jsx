aa3733ebb7ca8fea791d1a086690f1507739ac28
Mariano Majano Amaya <mariano@bu.edu>
2024-05-22 01:11:20 -0400

    Got the message layout working. For some reason, I can't navigate back to the profile when i click on it. I'll work on that now

--- frontend/src/Messages.jsx
+++ frontend/src/Messages.jsx
@@ -44,29 +44,70 @@ function Messages() {
     }
   };
 
+  const handleLogout = () => {
+    axios.get('http://localhost:8081/logout')
+      .then(res => {
+        navigate('/start');
+      }).catch(err => console.log(err));
+  };
+
+  
+
   return (
-    <div className='messages-container'>
-      <h2>Messages</h2>
-      <div className='messages-list'>
-        {messages.map((msg, index) => (
-          <div key={index} className='message-item'>
-            <strong>{msg.senderName}:</strong> {msg.content}
+    <div className="container-fluid">
+      <div className="row flex-nowrap">
+        <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
+          <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+            <a href="#" onClick={() => navigate(`/employeedetail/${id}`)} className="d-flex align-items-center pb-3 mb-md-1 mt-md-3 me-md-auto text-white text-decoration-none">
+              <span className="fs-5 fw-bolder d-none d-sm-inline">Employee Dashboard</span>
+            </a>
+            <ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+              <li>
+                <a href="#" onClick={() => navigate(`/employeedetail/${id}`)} className="nav-link text-white px-0 align-middle">
+                  <i className="fs-4 bi-speedometer2"></i> <span className="ms-1 d-none d-sm-inline">Profile</span>
+                </a>
+              </li>
+              <li>
+                <a href="#" onClick={() => navigate(`/employeedetail/${id}/messages`)} className="nav-link text-white px-0 align-middle">
+                  <i className="fs-4 bi-chat"></i> <span className="ms-1 d-none d-sm-inline">Messages</span>
+                </a>
+              </li>
+              <li onClick={handleLogout}>
+                <a href="#" className="nav-link px-0 align-middle text-white">
+                  <i className="fs-4 bi-power"></i> <span className="ms-1 d-none d-sm-inline">Logout</span></a>
+              </li>
+            </ul>
           </div>
-        ))}
-      </div>
-      <div className='message-form'>
-        <select value={selectedAdmin} onChange={(e) => setSelectedAdmin(e.target.value)} className='form-control mb-3'>
-          {admins.map(admin => (
-            <option key={admin.id} value={admin.id}>{admin.email}</option>
-          ))}
-        </select>
-        <textarea 
-          value={newMessage} 
-          onChange={(e) => setNewMessage(e.target.value)} 
-          className='form-control mb-3' 
-          placeholder='Type your message here...'
-        />
-        <button onClick={handleSendMessage} className='btn btn-primary'>Send Message</button>
+        </div>
+        <div className="col p-0 m-0">
+          <div className="p-2 d-flex justify-content-center shadow">
+            <h4>Employee Management System</h4>
+          </div>
+          <div className='d-flex justify-content-center flex-column align-items-center mt-3'>
+            <h2>Messages</h2>
+            <div className='messages-list'>
+              {messages.map((msg, index) => (
+                <div key={index} className='message-item'>
+                  <strong>{msg.senderName}:</strong> {msg.content}
+                </div>
+              ))}
+            </div>
+            <div className='message-form'>
+              <select value={selectedAdmin} onChange={(e) => setSelectedAdmin(e.target.value)} className='form-control mb-3'>
+                {admins.map(admin => (
+                  <option key={admin.id} value={admin.id}>{admin.email}</option>
+                ))}
+              </select>
+              <textarea 
+                value={newMessage} 
+                onChange={(e) => setNewMessage(e.target.value)} 
+                className='form-control mb-3' 
+                placeholder='Type your message here...'
+              />
+              <button onClick={handleSendMessage} className='btn btn-primary'>Send Message</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
